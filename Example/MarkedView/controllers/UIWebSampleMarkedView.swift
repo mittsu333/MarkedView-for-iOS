fa7db65a788127b14d887cb2035bb98eb80ca628
mittsu <otok553@gmail.com>
2016-04-14 02:08:15 +0900

    pod file update.

--- Example/MarkedView/controllers/UIWebSampleMarkedView.swift
+++ Example/MarkedView/controllers/UIWebSampleMarkedView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MarkedView
 
 class UIWebSampleMarkedView: UIViewController {
     
--- Example/MarkedView/controllers/WKWebSampleMarkedView.swift
+++ Example/MarkedView/controllers/WKWebSampleMarkedView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MarkedView
 
 class WKWebSampleMarkedView: UIViewController {
     
